4394a3fe38f2609fec8d305055818988341740e6
oldjang <oldjang@outlook.com>
2020-07-26 10:52:52 +0800

    Connect done

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,21 +8,17 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 }
 
+/*
+控制连接服务器
+*/
 bool MainWindow::controlConnect(){
     WSADATA wsaData;
     ::WSAStartup( MAKEWORD(2, 2), &wsaData);
     socketControl = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    if(socketControl== INVALID_SOCKET){
-           infoMessage = &"creatsocket error:"[errno];
-           qDebug("creatsocket error ");
-           ui->informationText->append("creat socket error");
-           return true;
-        }
-
     /*
-    连接信息
-    */
+     * 连接信息
+     */
     addrMessage = ui->addressText->text();//找出ip地址
     memset(&serverAddr,0,sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
@@ -59,9 +55,120 @@ bool MainWindow::controlConnect(){
     return true;
 }
 
+/*
+ * 用户登录
+ */
+
+bool MainWindow::FTPLogin()
+{
+    usernameMessage = ui->usernameText->text();
+    passwordMessage = ui->passwordText->text();
+    memset(&Sendbuf,0,sizeof(Sendbuf));
+    memset(&Respond,0,sizeof(Respond));
+    memcpy(Sendbuf,"USER ",strlen("USER "));
+    memcpy(Sendbuf+strlen("USER "),usernameMessage.toStdString().data(),strlen(usernameMessage.toStdString().data()));
+    memcpy(Sendbuf+strlen("USER ")+strlen(usernameMessage.toStdString().data()),"\r\n",2);
+    if(send(socketControl,Sendbuf,strlen(Sendbuf),0) == SOCKET_ERROR){
+        qDebug("socket username send error");
+        ui->informationText->append(&"socket username send error "[errno]);
+        return false;
+    }
+
+    ui->informationText->append("socket username send success");
+    if(recv(socketControl,Respond,MAXSIZE,0) == SOCKET_ERROR){
+        qDebug("socket username receive error");
+        ui->informationText->append("socket username receive error");
+    }
+    ui->informationText->append(Respond);
+
+    ui->informationText->append("password login...");
+    memset(&Sendbuf,0,sizeof(Sendbuf));
+    memset(&Respond,0,sizeof(Respond));
+    memcpy(Sendbuf,"PASS ",strlen("PASS "));
+    memcpy(Sendbuf+strlen("PASS "),passwordMessage.toStdString().data(),strlen(passwordMessage.toStdString().data()));
+    memcpy(Sendbuf+strlen("PASS ")+strlen(passwordMessage.toStdString().data()),"\r\n",2);
+    if(send(socketControl,Sendbuf,strlen(Sendbuf),0)<0){
+        qDebug("socket password send error");
+         ui->informationText->append("socket password send error");
+    }
+    ui->informationText->append("socket password send success ");
+    if(recv(socketControl,Respond,MAXSIZE,0)<0){
+        qDebug("socket password receive error");
+        ui->informationText->append("socket password receive error");
+    }
+    ui->informationText->append(Respond);
+
+    return true;
+}
+
+/*
+ * 数据连接
+ */
+
+bool MainWindow::dataConnect(){
+    socketConnect = socket(AF_INET,SOCK_STREAM,0);
+
+    socketData = socket(AF_INET,SOCK_STREAM,0);
+
+    dataAddr.sin_family=AF_INET;
+    dataAddr.sin_port = htons(p1*256+p2);
+    dataAddr.sin_addr.s_addr=inet_addr(addrMessage.toStdString().data());
+
+    //bind
+    if(bind(socketConnect,(struct sockaddr*)&dataAddr,sizeof(dataAddr)) == SOCKET_ERROR){
+        qDebug("bind error");
+        ui->informationText->append("bind error");
+        return false;
+    }
+    ui->informationText->append("bind client dataAddr success");
+    if(listen(socketConnect,3) < 0){
+        qDebug("listen error");
+        ui->informationText->append("listen error");
+        return false;
+    }
+    return true;
+}
+
+
+bool MainWindow::portRequest()
+{
+    ui->informationText->append("PORT....");
+
+   //PORT Request
+   memset(Sendbuf,0,sizeof(Sendbuf));
+   memcpy(Sendbuf,"PORT 127,0,0,1,20,80\r\n",strlen("PORT 127,0,0,1,20,80\r\n"));
+
+   if(send(socketControl,Sendbuf,strlen(Sendbuf),0)<0){
+       qDebug("PORT Request Error");
+       ui->informationText->append("PORT Request Error");
+       return true;
+   }
+   ui->informationText->append(Sendbuf);
+   memset(&Sendbuf,0,sizeof(Sendbuf));
+   memset(&Respond,0,sizeof(Respond));
+   if(recv(socketControl,Respond,MAXSIZE,0) == SOCKET_ERROR){
+       qDebug("receive portmessage error");
+       ui->informationText->append("receive portmessage error");
+       return false;
+   }
+   ui->informationText->append(Respond);
+   return true;
+}
+
+
 void MainWindow::on_connectButton_clicked()
 {
     controlConnect();
+}
+
+void MainWindow::on_loginButton_clicked()
+{
+    FTPLogin();
+    ui->informationText->append("---------------------------");
+    dataConnect();
+    portRequest();
+    QMessageBox::information(NULL, "login", "login success\r\nmodal:主动模式(PORT)", QMessageBox::Yes);
+
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <WinSock2.h>
 #include <ws2tcpip.h>
+#include <string.h>
 #include <QtWidgets/QMessageBox>
 
 #define MAXSIZE 1024
@@ -26,19 +27,28 @@ public:
 
 private slots:
     void on_connectButton_clicked();
+    void on_loginButton_clicked();
 
 private:
     Ui::MainWindow *ui;
 
     QString infoMessage;//信息
     QString addrMessage;//地址
+    QString usernameMessage;//用户名
+    QString passwordMessage;//密码
 
+    SOCKET socketData;//数据传输socket
+    SOCKET socketConnect;//连接socket
     SOCKET socketControl;//控制传输socket
     struct sockaddr_in serverAddr;//服务器地址
+    struct sockaddr_in dataAddr;//数据地址
 
     char Respond[MAXSIZE];//接收缓存
     char Sendbuf[MAXSIZE];//发送缓存
 
-    bool controlConnect();
+    bool controlConnect();//控制连接
+    bool FTPLogin();//登录
+    bool dataConnect();//数据连接
+    bool portRequest();
 };
 #endif // MAINWINDOW_H
